b1afbbfec0e85a55e69b128e352907d8af8f9281
Aakash3377 <85490359+Aakash3377@users.noreply.github.com>
2021-06-12 12:56:14 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,12 @@
+#age calculator
+#yearbirth=int(input("Enter your birth year:"))
+#age = 2021 - yearbirth
+#print("age is ",age)
+
+#simple calculator
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+print("a+b={}".format(a+b))
+print("a-b={}".format(a-b))
+print("a*b={}".format(a*b))
+print("a/b={}".format(a/b))
