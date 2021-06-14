257c4f862175700aa242800aab8a4dd96682c66b
Aakash3377 <85490359+Aakash3377@users.noreply.github.com>
2021-06-14 17:12:51 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,10 @@
+st =input("enter a any word:")
+print(st.count("y"))
+
+#take input string and print even index charcters
+st=input("enter a any word:")
+print(st[::2])
+
+#swap
+st ="EdUyEaR"
+print(st.swapcase())
